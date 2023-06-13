b8c814276b3f4aafb29abb6a1ad7df2e774d1b30
Shaylla Sthefany <shayllasthefany@gmail.com>
2023-06-13 15:49:28 -0400

    terceiro commit prova

--- prova 2° bimestre/Correcao.cs
+++ prova 2° bimestre/Correcao.cs
@@ -25,29 +25,26 @@ namespace prova_2__bimestre
 
         private void bt_avaliar_Click(object sender, EventArgs e)
         {
+            CorrecaoProva();
+        }
+
+        private void CorrecaoProva()
+        {
             string[] gabarito = { "A", "B", "D", "E", "C", "A", "A", "D", "E", "B" };
 
-            string[]resposta = {Convert.ToString(tx_1.Text.ToUpper()), Convert.ToString(tx_2.Text.ToUpper()), Convert.ToString(tx_3.Text.ToUpper()), Convert.ToString(tx_4.Text.ToUpper()), Convert.ToString(tx_5.Text.ToUpper()), Convert.ToString(tx_6.Text.ToUpper()), Convert.ToString(tx_7.Text.ToUpper()), Convert.ToString(tx_8.Text.ToUpper()), Convert.ToString(tx_9.Text.ToUpper()), Convert.ToString(tx_10.Text.ToUpper())};
+            string[] resposta = { Convert.ToString(tx_1.Text.ToUpper()), Convert.ToString(tx_2.Text.ToUpper()), Convert.ToString(tx_3.Text.ToUpper()), Convert.ToString(tx_4.Text.ToUpper()), Convert.ToString(tx_5.Text.ToUpper()), Convert.ToString(tx_6.Text.ToUpper()), Convert.ToString(tx_7.Text.ToUpper()), Convert.ToString(tx_8.Text.ToUpper()), Convert.ToString(tx_9.Text.ToUpper()), Convert.ToString(tx_10.Text.ToUpper()) };
 
             int qtdacertos = 0;
 
-            for (int i = 0;i < gabarito.Length; i++)
+            for (int i = 0; i < gabarito.Length; i++)
             {
                 if (resposta[i] == gabarito[i])
                 {
                     qtdacertos += 1;
                 }
             }
-            MessageBox.Show(Convert.ToString(qtdacertos));
 
-
-
-
-
-
-
-
-
+            label2.Text = qtdacertos.ToString();
         }
 
         private void tx_1_TextChanged(object sender, EventArgs e)
